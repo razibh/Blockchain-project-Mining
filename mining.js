ab7b203f96329444a72d687b36462840cdce4394
razibh <razib11@yahoo.com>
2022-12-05 20:08:08 +0600

    first commit

---
+++ mining.js
@@ -0,0 +1,68 @@
+const sha256 = require("crypto-js/sha256")
+class Block{
+    constructor(timestamp,data, previoushash = ""){
+        this.timestamp = timestamp;
+
+        this.data = data;
+        this.previoushash = previoushash;
+        this.hash = this.calculateHash();
+        this.nonce =0;
+    }
+    mineBlock(difficulty)
+    {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty +1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Mining DOne : "+ this.hash);
+    }
+    calculateHash(){
+        return sha256( this.timestamp + JSON.stringify(this.data) + this
+        .previoushash + this.nonce).toString();
+    }
+}
+
+class Blockchain {
+    constructor (){
+        this.chain = [this.generateGenesisBlocl()];
+        this.difficulty =2;
+}
+generateGenesisBlocl(){
+    return new Block("2022-12-05", "GENESIS", "0000 ");
+}
+    getLatestBlock(){
+        return this.chain[this.chain.length -1];
+    }
+    addBlock(newBlock){
+        newBlock.previoushash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    
+}
+    isBlockchainValid(){
+        for (let i=1; i<this.chain.length; i++)
+        {
+            const  CurrantBlock = this.chain[i];
+            const previoushBlock = this.chain[i-1];
+            if ( CurrantBlock.hash !== CurrantBlock.calculateHash())
+            {
+                return false;
+            }
+            if (CurrantBlock.previoushash !== previoushBlock.hash)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    }
+
+const josscoin = new Blockchain();
+const block =new Block("2022-12-04", { amount: 5 }, "ABCD");
+josscoin.addBlock(block);
+console.log(josscoin.isBlockchainValid());
+ josscoin.chain[1].data = "Haked";
+ console.log(josscoin.isBlockchainValid());
+ 
+
+ 
